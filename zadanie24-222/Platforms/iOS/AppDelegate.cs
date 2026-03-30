45ab5095f23044b504276c1768cbf08dd2a98b50
bombiarzini <kacperdragon025@gmail.com>
2026-03-30 13:51:58 +0200

    zadanie 24.2

---
+++ zadanie24-222/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace zadanie24_222
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
